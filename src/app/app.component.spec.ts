637c2780905bfe28fbf6b2e8a11b6204c96f81a1
Nacho Vazquez <nachovc1410@gmail.com>
2020-07-20 02:27:32 -0300

    feat: 🎸 add usage example to app.component (#2)
    * feat: 🎸 add usage example to app.component
    
    * fix: 🐛 failing tests
    
    Co-authored-by: Nacho Vazquez <nacho.vazquez.dev@gmail.com>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ConsoleDriverModule, LumberjackModule } from '@ngworker/lumberjack';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
+      imports: [LumberjackModule.forRoot(), ConsoleDriverModule.forRoot()],
     }).compileComponents();
   }));
 
