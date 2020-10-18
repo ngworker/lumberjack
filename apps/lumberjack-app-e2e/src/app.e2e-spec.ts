fc857e9b7f9b172545682a054928bb0acf3a5b39
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-10-19 00:47:03 +0200

    test: fix e2e tests

--- apps/lumberjack-app-e2e/src/app.e2e-spec.ts
+++ apps/lumberjack-app-e2e/src/app.e2e-spec.ts
@@ -17,7 +17,14 @@ describe('workspace-project App', () => {
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(
+    const filteredLogs = logs.filter(
+      (log) =>
+        !log.message.includes('[AppComponent]') &&
+        !log.message.includes('/api/logs') &&
+        !log.message.includes('polyfills.js')
+    );
+    // tslint:disable-next-line: no-floating-promises
+    expect(filteredLogs).not.toContain(
       jasmine.objectContaining({
         level: logging.Level.SEVERE,
       } as logging.Entry)
