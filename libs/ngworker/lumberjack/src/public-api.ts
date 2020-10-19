e92ef31423ea43b17b6844c10154c5ebdca59fef
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-10-19 21:45:38 +0200

    refactor: organize public API

--- libs/ngworker/lumberjack/src/public-api.ts
+++ libs/ngworker/lumberjack/src/public-api.ts
@@ -1,12 +1,12 @@
 /*
- * Public API Surface of lumberjack
+ * Public API Surface of @ngworker/lumberjack
  */
 
+export * from './lib/configs';
+export * from './lib/log-creator';
+export * from './lib/log-drivers';
+export * from './lib/log-types';
+export * from './lib/lumberjack-log';
+export * from './lib/lumberjack-log-levels';
 export * from './lib/lumberjack.module';
 export * from './lib/lumberjack.service';
-export * from './lib/log-drivers';
-export * from './lib/log-creator';
-export * from './lib/log-types';
-export * from './lib/configs';
-export * from './lib/lumberjack-log-levels';
-export * from './lib/lumberjack-log';
