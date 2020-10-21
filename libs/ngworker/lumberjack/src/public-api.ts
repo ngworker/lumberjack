a671c0b686e5be352510470c3749c531ce03d02b
Nacho Vazquez <nachovc1410@gmail.com>
2020-10-20 21:03:17 -0300

    fix: export needed type `LumberjackLogLevelComposable` in public_api
    Co-authored-by: Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>

--- libs/ngworker/lumberjack/src/public-api.ts
+++ libs/ngworker/lumberjack/src/public-api.ts
@@ -7,6 +7,6 @@ export * from './lib/log-creator';
 export * from './lib/log-drivers';
 export * from './lib/log-types';
 export * from './lib/lumberjack-log';
-export { LumberjackLogLevel } from './lib/lumberjack-log-levels';
+export * from './lib/lumberjack-log-levels';
 export * from './lib/lumberjack.module';
 export * from './lib/lumberjack.service';
