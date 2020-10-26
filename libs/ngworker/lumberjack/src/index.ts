a5e61f5260c0e2dba313bac41f9540b3bef3bd73
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-10-26 23:12:19 +0100

    fix: expose LumberjackLogger

--- libs/ngworker/lumberjack/src/index.ts
+++ libs/ngworker/lumberjack/src/index.ts
@@ -6,6 +6,7 @@ export * from './lib/configs';
 export * from './lib/log-drivers';
 export * from './lib/lumberjack-log';
 export * from './lib/lumberjack-log-levels';
+export * from './lib/lumberjack-logger.service';
 export * from './lib/lumberjack.module';
 export * from './lib/lumberjack.service';
 export * from './lib/time';
