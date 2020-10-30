1a88bd9167f05c65b4e6b9e824c318e7a56703d8
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-10-31 00:11:04 +0100

    docs: remove v1 artifacts from app

--- apps/lumberjack-app/src/app/log-types/error-logs/app.component.log.ts
+++
@@ -1,7 +0,0 @@
-import { createErrorLog, LumberjackLog } from '@ngworker/lumberjack';
-
-function createAppErrorLog(message: string): () => LumberjackLog {
-  return createErrorLog(message, 'AppComponent');
-}
-
-export const ForestOnFire = createAppErrorLog('The forest is on fire');
--- apps/lumberjack-app/src/app/log-types/info-logs/app.component.log.ts
+++
@@ -1,7 +0,0 @@
-import { createInfoLog, LumberjackLog } from '@ngworker/lumberjack';
-
-function createAppInfoLog(message: string): () => LumberjackLog {
-  return createInfoLog(message, 'AppComponent');
-}
-
-export const HelloForest = createAppInfoLog('HelloForest');
