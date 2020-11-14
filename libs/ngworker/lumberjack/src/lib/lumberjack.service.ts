a22695dd958e74debd4c7be111097e51a396e9ed
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-11-14 14:13:42 +0100

    fix: handle error-throwing log drivers

--- libs/ngworker/lumberjack/src/lib/lumberjack.service.ts
+++ libs/ngworker/lumberjack/src/lib/lumberjack.service.ts
@@ -33,7 +33,11 @@ export class LumberjackService {
 
     for (const logDriver of this.logDrivers) {
       if (this.canDriveLog(logDriver, logItem.level)) {
-        this.logToTheRightLevel(logDriver, logItem, format);
+        try {
+          this.logToTheRightLevel(logDriver, logItem, format);
+        } catch (error) {
+          this.logDriverError(logDriver, logItem, format, error);
+        }
       }
     }
   }
@@ -44,6 +48,20 @@ export class LumberjackService {
       (driver.config.levels.length === 1 && driver.config.levels[0] === LumberjackLogLevel.Verbose) ||
       (driver.config.levels as LumberjackLogEntryLevel[]).includes(level)
     );
+  }
+
+  private logDriverError(
+    driver: LogDriver,
+    logEntry: LumberjackLog,
+    format: (logEntry: LumberjackLog) => string,
+    error: unknown
+  ): void {
+    const thrownErrorMessage = (error as Error).message || String(error);
+    const errorMessage = `Could not log message "${format(logEntry)}" to ${
+      // tslint:disable-next-line: no-any
+      (driver as any).name || 'the specificied log driver'
+    }. Error: ${thrownErrorMessage}`;
+    console.error(errorMessage);
   }
 
   private logToTheRightLevel(
