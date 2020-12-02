309f0775c8f6ef26b9cc4c2d27d252780f3937af
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-12-02 22:07:06 +0100

    refactor: delete unused file

--- libs/ngworker/lumberjack/src/lib/configuration/lumberjack-log-config.token.ts
+++
@@ -1,7 +0,0 @@
-import { InjectionToken } from '@angular/core';
-
-import { LumberjackConfig } from './lumberjack.config';
-
-export const lumberjackLogConfigToken: InjectionToken<LumberjackConfig> = new InjectionToken(
-  '__LUMBERJACK_LOG_CONFIG__'
-);
