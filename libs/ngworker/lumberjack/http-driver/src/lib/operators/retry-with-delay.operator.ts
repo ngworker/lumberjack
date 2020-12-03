3e7713f50bbb294aa9c742839aaa135e4b4c3dde
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-12-03 01:25:14 +0100

    feat: correct error text in operator

--- libs/ngworker/lumberjack/http-driver/src/lib/operators/retry-with-delay.operator.ts
+++ libs/ngworker/lumberjack/http-driver/src/lib/operators/retry-with-delay.operator.ts
@@ -3,5 +3,5 @@ import { delay, retryWhen, take } from 'rxjs/operators';
 
 export const retryWithDelay = <T>(maxRetries: number, delayMs: number) =>
   retryWhen<T>((errors) =>
-    concat(errors.pipe(delay(delayMs), take(maxRetries + 1)), throwError(`Failed after ${maxRetries} maxRetries.`))
+    concat(errors.pipe(delay(delayMs), take(maxRetries + 1)), throwError(`Failed after ${maxRetries} retries.`))
   );
