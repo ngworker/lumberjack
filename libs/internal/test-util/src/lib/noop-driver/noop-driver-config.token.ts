ff5c997b71f60c267a1073abfd329dbc75687246
Nacho Vazquez <nachovc1410@gmail.com>
2020-12-20 15:46:25 -0300

    fix: 🐛 remove cyclic dependencies

--- libs/internal/test-util/src/lib/noop-driver/noop-driver-config.token.ts
+++ libs/internal/test-util/src/lib/noop-driver/noop-driver-config.token.ts
@@ -2,8 +2,6 @@ import { inject, InjectionToken } from '@angular/core';
 
 import { LumberjackLogDriverConfig, lumberjackLogDriverConfigToken } from '@ngworker/lumberjack';
 
-import { NoopDriver } from './noop.driver';
-
 export const noopDriverConfigToken = new InjectionToken<LumberjackLogDriverConfig>('__NO-OP_DRIVER_CONFIG__', {
-  factory: () => ({ ...inject(lumberjackLogDriverConfigToken), identifier: NoopDriver.name }),
+  factory: () => ({ ...inject(lumberjackLogDriverConfigToken), identifier: 'NoopDriver' }),
 });
--- libs/internal/test-util/src/lib/spy-driver/spy-driver-config.token.ts
+++ libs/internal/test-util/src/lib/spy-driver/spy-driver-config.token.ts
@@ -2,8 +2,6 @@ import { inject, InjectionToken } from '@angular/core';
 
 import { LumberjackLogDriverConfig, lumberjackLogDriverConfigToken } from '@ngworker/lumberjack';
 
-import { SpyDriver } from './spy.driver';
-
 export const spyDriverConfigToken = new InjectionToken<LumberjackLogDriverConfig>('__SPY_DRIVER_CONFIG__', {
-  factory: () => ({ ...inject(lumberjackLogDriverConfigToken), identifier: SpyDriver.name }),
+  factory: () => ({ ...inject(lumberjackLogDriverConfigToken), identifier: 'SpyDriver' }),
 });
