f51c28464acd95d188350359a4cc3c59f3413da5
Nacho Vazquez <nachovc1410@gmail.com>
2021-01-08 23:50:28 -0300

    test: 💍 add lumberjack log builder test suite

--- libs/ngworker/lumberjack/src/lib/logging/lumberjack-log.builder.ts
+++ libs/ngworker/lumberjack/src/lib/logging/lumberjack-log.builder.ts
@@ -3,29 +3,33 @@ import { LumberjackLogPayload } from '../logs/lumberjack-log-payload';
 import { LumberjackLog } from '../logs/lumberjack.log';
 import { LumberjackTimeService } from '../time/lumberjack-time.service';
 
+type Empty = 'empty' & LumberjackLogPayload;
+
 export class LumberjackLogBuilder<TPayload extends LumberjackLogPayload | void = void> {
   private payload?: TPayload;
   private scope?: string;
 
   constructor(private time: LumberjackTimeService, private level: LumberjackLogLevel, private message: string) {}
 
-  build(...payloadArg: TPayload extends void ? [never?] : [TPayload]): LumberjackLog<Exclude<TPayload, void>> {
+  build(
+    ...payloadArg: Extract<TPayload, Empty> extends never ? [TPayload] : [never?]
+  ): LumberjackLog<Exclude<TPayload, Empty>> {
     return {
       level: this.level,
       message: this.message,
       scope: this.scope,
       createdAt: this.time.getUnixEpochTicks(),
-      payload: (payloadArg[0] || this.payload) as Exclude<TPayload, void>,
+      payload: (payloadArg[0] || this.payload) as Exclude<TPayload, Empty>,
     };
   }
 
   /**
    * Add payload with custom data to the `LumberjackLog`
    */
-  withPayload(...payloadArg: TPayload extends void ? [never?] : [TPayload]): LumberjackLogBuilder<void | TPayload> {
+  withPayload(...payloadArg: TPayload extends void ? [never?] : [TPayload]): LumberjackLogBuilder<Empty | TPayload> {
     this.payload = payloadArg[0] as TPayload;
 
-    return (this as unknown) as LumberjackLogBuilder<void | TPayload>;
+    return (this as unknown) as LumberjackLogBuilder<Empty | TPayload>;
   }
 
   /**
