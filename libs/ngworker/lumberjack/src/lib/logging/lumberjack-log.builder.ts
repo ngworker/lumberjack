0c4a5ce052bc12dadcadb626872834b9e87c4bde
Nacho Vazquez <nachovc1410@gmail.com>
2021-01-09 23:02:17 -0300

    docs: ✏️ document util type that determines withPayload usage

--- libs/ngworker/lumberjack/src/lib/logging/lumberjack-log.builder.ts
+++ libs/ngworker/lumberjack/src/lib/logging/lumberjack-log.builder.ts
@@ -3,7 +3,10 @@ import { LumberjackLogPayload } from '../logs/lumberjack-log-payload';
 import { LumberjackLog } from '../logs/lumberjack.log';
 import { LumberjackTimeService } from '../time/lumberjack-time.service';
 
-type Empty = 'empty' & LumberjackLogPayload;
+/**
+ * Util type that helps to identify if the `withPayload` method was used.
+ */
+type StaticPayload = 'static-payload' & LumberjackLogPayload;
 
 export class LumberjackLogBuilder<TPayload extends LumberjackLogPayload | void = void> {
   private payload?: TPayload;
@@ -12,24 +15,26 @@ export class LumberjackLogBuilder<TPayload extends LumberjackLogPayload | void =
   constructor(private time: LumberjackTimeService, private level: LumberjackLogLevel, private message: string) {}
 
   build(
-    ...payloadArg: Extract<TPayload, Empty> extends never ? [TPayload] : [never?]
-  ): LumberjackLog<Exclude<TPayload, Empty>> {
+    ...payloadArg: Extract<TPayload, StaticPayload> extends never ? [TPayload] : [never?]
+  ): LumberjackLog<Exclude<TPayload, StaticPayload>> {
     return {
       level: this.level,
       message: this.message,
       scope: this.scope,
       createdAt: this.time.getUnixEpochTicks(),
-      payload: (payloadArg[0] || this.payload) as Exclude<TPayload, Empty>,
+      payload: (payloadArg[0] || this.payload) as Exclude<TPayload, StaticPayload>,
     };
   }
 
   /**
    * Add payload with custom data to the `LumberjackLog`
    */
-  withPayload(...payloadArg: TPayload extends void ? [never?] : [TPayload]): LumberjackLogBuilder<Empty | TPayload> {
+  withPayload(
+    ...payloadArg: TPayload extends void ? [never?] : [TPayload]
+  ): LumberjackLogBuilder<StaticPayload | TPayload> {
     this.payload = payloadArg[0] as TPayload;
 
-    return (this as unknown) as LumberjackLogBuilder<Empty | TPayload>;
+    return (this as unknown) as LumberjackLogBuilder<StaticPayload | TPayload>;
   }
 
   /**
