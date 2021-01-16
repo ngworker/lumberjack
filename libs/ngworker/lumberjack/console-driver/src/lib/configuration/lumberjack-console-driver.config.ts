9bcafef83efec8b2a84cb444cfa7e5a01260df64
Nacho Vazquez <nachovc1410@gmail.com>
2021-01-16 19:15:53 -0300

    fix: 🐛 clean previous commit issues

--- libs/ngworker/lumberjack/console-driver/src/lib/configuration/lumberjack-console-driver.config.ts
+++ libs/ngworker/lumberjack/console-driver/src/lib/configuration/lumberjack-console-driver.config.ts
@@ -1,7 +1,4 @@
 import { LumberjackLogDriverConfig } from '@ngworker/lumberjack';
 
 export type LumberjackConsoleDriverConfig = Omit<LumberjackLogDriverConfig, 'identifier'> &
-  Partial<Pick<LumberjackLogDriverConfig, 'identifier'>>;;
   Partial<Pick<LumberjackLogDriverConfig, 'identifier'>>;
-
-export type LumberjackConsoleDriverConfig = LumberjackLogDriverConfigWithPartialIdentifer;
