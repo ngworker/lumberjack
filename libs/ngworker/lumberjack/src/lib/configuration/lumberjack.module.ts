ed50eef04bb54b4ba14d4e6c65bc5d91e60d4732
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-01-17 02:19:27 +0100

    docs: add inline docs to LumberjackModule#forRoot

--- libs/ngworker/lumberjack/src/lib/configuration/lumberjack.module.ts
+++ libs/ngworker/lumberjack/src/lib/configuration/lumberjack.module.ts
@@ -5,14 +5,20 @@ import { LumberjackRootModule } from './lumberjack-root.module';
 import { LumberjackOptions } from './lumberjack.options';
 
 /**
- * The Lumberjack Angular module is used to register required necessary
- * Lumberjack services and configure shared settings.
+ * The Lumberjack Angular module is used to register necessary Lumberjack
+ * services and configure shared settings.
  *
  * NOTE! Do not import `LumberjackModule` directly. Use
  * `LumberjackModule.forRoot`.
  */
 @NgModule()
 export class LumberjackModule {
+  /**
+   * Register necessary Lumberjack services and configure shared settings.
+   *
+   * @param options Settings shared throughout Lumberjack services and log
+   *   drivers.
+   */
   static forRoot(options?: LumberjackOptions): ModuleWithProviders<LumberjackRootModule> {
     return {
       ngModule: LumberjackRootModule,
