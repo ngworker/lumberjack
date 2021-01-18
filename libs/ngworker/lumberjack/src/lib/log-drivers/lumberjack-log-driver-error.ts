35b205a538491100ee44bd73e7c8d089cba7d0f8
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-01-18 20:49:55 +0100

    style: format

--- libs/ngworker/lumberjack/src/lib/log-drivers/lumberjack-log-driver-error.ts
+++ libs/ngworker/lumberjack/src/lib/log-drivers/lumberjack-log-driver-error.ts
@@ -7,20 +7,20 @@ import { LumberjackLogDriver } from './lumberjack-log-driver';
  * A log driver error reported by Lumberjack.
  */
 export interface LumberjackLogDriverError<TPayload extends LumberjackLogPayload | void = void> {
-/**
-* The text representation of the log passed to the driver at the moment of failure. 
-*/
+  /**
+   * The text representation of the log passed to the driver at the moment of failure.
+   */
   readonly formattedLog: string;
-/**
-* The error threw by the driver at the moment of failure.
-*/
+  /**
+   * The error threw by the driver at the moment of failure.
+   */
   readonly error: unknown;
-/**
-* The log passed to the driver at the moment of failure.
-*/
+  /**
+   * The log passed to the driver at the moment of failure.
+   */
   readonly log: LumberjackLog<TPayload>;
-/**
-* The failing driver.
-*/
+  /**
+   * The failing driver.
+   */
   readonly logDriver: LumberjackLogDriver<TPayload>;
 }
