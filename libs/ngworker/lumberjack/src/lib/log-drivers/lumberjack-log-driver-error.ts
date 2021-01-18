ad4af4feaee089cf0ed3c6c731e311ebc2713f9e
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-01-18 20:52:10 +0100

    docs: adjust JSDoc for LumberjackLogDriverError

--- libs/ngworker/lumberjack/src/lib/log-drivers/lumberjack-log-driver-error.ts
+++ libs/ngworker/lumberjack/src/lib/log-drivers/lumberjack-log-driver-error.ts
@@ -8,19 +8,20 @@ import { LumberjackLogDriver } from './lumberjack-log-driver';
  */
 export interface LumberjackLogDriverError<TPayload extends LumberjackLogPayload | void = void> {
   /**
-   * The text representation of the log passed to the driver at the moment of failure.
+   * The text representation of the log passed to the driver at the time of
+   * failure.
    */
   readonly formattedLog: string;
   /**
-   * The error threw by the driver at the moment of failure.
+   * The error thrown by the log driver at the time of failure.
    */
   readonly error: unknown;
   /**
-   * The log passed to the driver at the moment of failure.
+   * The log passed to the driver at the time of failure.
    */
   readonly log: LumberjackLog<TPayload>;
   /**
-   * The failing driver.
+   * The failing log driver.
    */
   readonly logDriver: LumberjackLogDriver<TPayload>;
 }
