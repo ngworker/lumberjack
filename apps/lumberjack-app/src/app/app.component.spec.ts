ce043a1206801d4915d029e6fa0f2bc64c3f40b3
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-01-19 23:27:43 +0100

    chore: remove Spectator to fix dependency issue

--- apps/lumberjack-app/src/app/app.component.spec.ts
+++ apps/lumberjack-app/src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { createComponentFactory, Spectator } from '@ngneat/spectator';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { NoopConsoleModule } from '@internal/console-driver/test-util';
 import { LumberjackLevel, LumberjackModule } from '@ngworker/lumberjack';
@@ -8,37 +9,43 @@ import { LumberjackHttpDriverModule } from '@ngworker/lumberjack/http-driver';
 
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
-  let spectator: Spectator<AppComponent>;
-  const createComponent = createComponentFactory({
-    component: AppComponent,
-    imports: [
-      HttpClientTestingModule,
-      LumberjackModule.forRoot(),
-      LumberjackConsoleDriverModule.forRoot(),
-      NoopConsoleModule,
-      LumberjackHttpDriverModule.forRoot({
-        levels: [LumberjackLevel.Error],
-        origin: 'ForestAPP',
-        storeUrl: '/api/logs',
-        retryOptions: { maxRetries: 5, delayMs: 250 },
-      }),
-    ],
-  });
-  beforeEach(() => {
-    spectator = createComponent();
+describe(AppComponent.name, () => {
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      imports: [
+        HttpClientTestingModule,
+        LumberjackModule.forRoot(),
+        LumberjackConsoleDriverModule.forRoot(),
+        NoopConsoleModule,
+        LumberjackHttpDriverModule.forRoot({
+          levels: [LumberjackLevel.Error],
+          origin: 'ForestAPP',
+          storeUrl: '/api/logs',
+          retryOptions: { maxRetries: 5, delayMs: 250 },
+        }),
+      ],
+    });
+    await TestBed.compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   it('should create the app', () => {
-    expect(spectator.component).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it(`should have as title 'lumberjack'`, () => {
-    expect(spectator.component.title).toEqual('lumberjack');
+    expect(component.title).toEqual('lumberjack');
   });
 
   it('should render title', () => {
-    const query = spectator.query('.content span');
+    const query: HTMLElement = fixture.debugElement.query(By.css('.content span')).nativeElement;
     expect(query && query.textContent).toContain('lumberjack app is running!');
   });
 });
