468fd97d21bf7b5b98a22fb093e2abeb27860999
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-01-25 21:45:08 +0100

    ci: [WIP]

--- karma.conf.js
+++ karma.conf.js
@@ -12,9 +12,7 @@ module.exports = () => ({
     require('karma-chrome-launcher'),
     require('karma-jasmine-html-reporter'),
     require('karma-coverage'),
-    require('karma-junit-reporter'),
     require('@angular-devkit/build-angular/plugins/karma'),
-    require('karma-sonarqube-unit-reporter'),
   ],
   client: {
     clearContext: false, // leave Jasmine Spec Runner output visible in browser
@@ -27,25 +25,7 @@ module.exports = () => ({
     subdir: '.',
     reporters: [{ type: 'text-summary' }, { type: 'lcovonly', subdir: '.', file: 'lcov.info' }],
   },
-  junitReporter: {
-    outputDir: path.join(__dirname, 'reports', 'test'), // results will be saved as $outputDir/$browserName.xml
-    outputFile: undefined, // if included, results will be saved as $outputDir/$browserName/$outputFile
-    suite: '', // suite will become the package name attribute in xml testsuite element
-    useBrowserName: false, // add browser name to report and classes names
-    nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
-    classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
-    properties: {}, // key value pair of properties to add to the <properties> section of the report
-    xmlVersion: 1, // use '1' if reporting to be per SonarQube 6.2 XML format
-  },
-  sonarQubeUnitReporter: {
-    sonarQubeVersion: 'LATEST',
-    outputFile: '../../../reports/test/sonar-lumberjack.xml',
-    useBrowserName: false,
-    // overrideTestDescription: true,
-    // testPaths: ['./src/app'],
-    // testFilePattern: '.spec.ts',
-  },
-  reporters: ['progress', 'kjhtml', 'junit', 'sonarqubeUnit'],
+  reporters: ['progress', 'kjhtml'],
   port: 9876,
   colors: true,
   logLevel: constants.LOG_INFO,
