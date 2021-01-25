76112beef5200990aded7462dd8f67bc60b65371
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-01-25 22:41:17 +0100

    ci: publish test coverage reports to SonarCloud

--- tools/generate-test-coverage-reports.mjs
+++ tools/generate-test-coverage-reports.mjs
@@ -1,18 +1,41 @@
 import { execSync } from 'child_process';
-import { readFileSync } from 'fs';
+import { readFileSync, renameSync } from 'fs';
+import * as path from 'path';
+import readdirp from 'readdirp';
 
-const workspace = JSON.parse(readFileSync('./angular.json').toString());
+async function flattenCoverageReports() {
+  const coverageDirectory = 'coverage';
 
-const hasTestCoverage = (project) =>
-  project.architect.test &&
-  project.architect.test.configurations &&
-  project.architect.test.configurations.coverage != null;
+  for await (const { path: filePath } of readdirp(coverageDirectory, { fileFilter: 'lcov.info' })) {
+    const flattenedFileName = flattenedCoverageReportFileName(filePath);
 
-Object.entries(workspace.projects)
-  .filter(([_projectName, project]) => hasTestCoverage(project))
-  .map(([projectName]) => projectName)
-  .forEach((projectName) => {
-    execSync(`ng test ${projectName} --configuration=coverage`, {
-      stdio: 'inherit',
+    renameSync(path.join(coverageDirectory, filePath), path.join(coverageDirectory, flattenedFileName));
+  }
+}
+
+function flattenedCoverageReportFileName(reportPath) {
+  return reportPath.replace(new RegExp('\\' + path.sep, 'g'), '-');
+}
+
+function runTestCoverageTargets() {
+  const workspace = JSON.parse(readFileSync('./angular.json').toString());
+
+  const hasTestCoverage = (project) =>
+    project.architect.test &&
+    project.architect.test.configurations &&
+    project.architect.test.configurations.coverage != null;
+
+  Object.entries(workspace.projects)
+    .filter(([_projectName, project]) => hasTestCoverage(project))
+    .map(([projectName]) => projectName)
+    .forEach((projectName) => {
+      execSync(`ng test ${projectName} --configuration=coverage`, {
+        stdio: 'inherit',
+      });
     });
-  });
+}
+
+(async () => {
+  runTestCoverageTargets();
+  await flattenCoverageReports();
+})();
