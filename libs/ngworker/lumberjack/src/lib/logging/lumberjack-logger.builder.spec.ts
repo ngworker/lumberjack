0834bca94571d5d09ebd269ce6f47e88ec142db5
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-01-31 02:08:52 +0100

    test: import missing type

--- libs/ngworker/lumberjack/src/lib/logging/lumberjack-logger.builder.spec.ts
+++ libs/ngworker/lumberjack/src/lib/logging/lumberjack-logger.builder.spec.ts
@@ -6,6 +6,7 @@ import { LumberjackModule } from '../configuration/lumberjack.module';
 import { LumberjackLevel } from '../logs/lumberjack-level';
 import { LumberjackLogLevel } from '../logs/lumberjack-log-level';
 import { LumberjackLogPayload } from '../logs/lumberjack-log-payload';
+import { LumberjackLog } from '../logs/lumberjack.log';
 import { LumberjackTimeService } from '../time/lumberjack-time.service';
 
 import { LumberjackLogBuilder } from './lumberjack-log.builder';
