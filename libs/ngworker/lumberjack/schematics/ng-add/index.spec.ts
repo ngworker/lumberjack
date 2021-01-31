6188eaba5b73164948290a51f51fdbe3dc401c77
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-01-31 22:20:29 +0100

    test: cover console driver

--- libs/ngworker/lumberjack/schematics/ng-add/index.spec.ts
+++ libs/ngworker/lumberjack/schematics/ng-add/index.spec.ts
@@ -48,7 +48,7 @@ describe('@ngworker/lumberjack:ng-add schematic', () => {
   let schematicRunner: SchematicTestRunner;
   let options: NgAddOptions;
 
-  describe('LumberjackModule', () => {
+  describe('Lumberjack', () => {
     it('imports LumberjackModule in the specified ES module', async () => {
       const tree = await schematicRunner.runSchematicAsync('ng-add', options, appTree).toPromise();
 
@@ -60,7 +60,51 @@ describe('@ngworker/lumberjack:ng-add schematic', () => {
       const tree = await schematicRunner.runSchematicAsync('ng-add', options, appTree).toPromise();
 
       const content = readModuleFile(tree, options.project, options.module || '');
-      expect(content).toMatch(/\s+imports:\s*\[\s*BrowserModule,\s*LumberjackModule.forRoot()/);
+      expect(content).toMatch(/\s+imports:\s*\[\s*BrowserModule,\s*LumberjackModule.forRoot\(\)/);
+    });
+  });
+
+  describe('Console driver', () => {
+    const consoleDriverImportPattern = /\s+imports:\s*\[\s*BrowserModule,\s*LumberjackModule.forRoot\(\),\s*LumberjackConsoleDriverModule.forRoot\(\)/;
+
+    it('imports LumberjackConsoleDriverModule in the specified ES module', async () => {
+      const tree = await schematicRunner.runSchematicAsync('ng-add', options, appTree).toPromise();
+
+      const content = readModuleFile(tree, options.project, options.module || '');
+      expect(content).toMatch(
+        /\nimport { LumberjackConsoleDriverModule } from '@ngworker\/lumberjack\/console-driver';/
+      );
+    });
+
+    it('registers the console driver in the specified Angular module by default', async () => {
+      const tree = await schematicRunner.runSchematicAsync('ng-add', options, appTree).toPromise();
+
+      const content = readModuleFile(tree, options.project, options.module || '');
+      expect(content).toMatch(consoleDriverImportPattern);
+    });
+
+    it('registers the console driver in the specified Angular module when the "consoleDriver" option is true', async () => {
+      options = {
+        ...options,
+        consoleDriver: true,
+      };
+
+      const tree = await schematicRunner.runSchematicAsync('ng-add', options, appTree).toPromise();
+
+      const content = readModuleFile(tree, options.project, options.module || '');
+      expect(content).toMatch(consoleDriverImportPattern);
+    });
+
+    it('does not register the console driver in the specified Angular module when the "consoleDriver" option is false', async () => {
+      options = {
+        ...options,
+        consoleDriver: false,
+      };
+
+      const tree = await schematicRunner.runSchematicAsync('ng-add', options, appTree).toPromise();
+
+      const content = readModuleFile(tree, options.project, options.module || '');
+      expect(content).not.toMatch(consoleDriverImportPattern);
     });
   });
 });
