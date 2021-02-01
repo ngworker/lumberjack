55f93fc244b0b61c3c6dfe4a369808cbffd7a7b8
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-02-01 23:33:22 +0100

    ci: fix test coverage reporting

--- libs/ngworker/lumberjack/jest.config.js
+++ libs/ngworker/lumberjack/jest.config.js
@@ -2,7 +2,7 @@ const baseConfig = require('../../../jest.config');
 
 module.exports = {
   ...baseConfig,
-  coverageReporters: ['text-summary', ['lcovonly', { subdir: '.', file: 'lcov-ngworker-lumberjack.info' }]],
+  coverageReporters: ['text-summary', ['lcovonly', { subdir: '.', file: 'ngworker-lumberjack-lcov.info' }]],
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/libs/ngworker/lumberjack/tsconfig.spec.json',
