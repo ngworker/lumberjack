54590fee3c069193f7f23a65437bb2db37ed5824
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-02-01 22:49:07 +0100

    chore: configure Jest and TypeScript

--- libs/internal/console-driver/test-util/jest.config.js
+++ libs/internal/console-driver/test-util/jest.config.js
@@ -2,7 +2,7 @@ const baseConfig = require('../../../../jest.config');
 
 module.exports = {
   ...baseConfig,
-  coverageReporters: ['text-summary', 'lcovonly'],
+  coverageReporters: ['text-summary'],
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/libs/internal/console-driver/test-util/tsconfig.spec.json',
--- libs/internal/test-util/jest.config.js
+++ libs/internal/test-util/jest.config.js
@@ -2,7 +2,7 @@ const baseConfig = require('../../../jest.config');
 
 module.exports = {
   ...baseConfig,
-  coverageReporters: ['text-summary', 'lcovonly'],
+  coverageReporters: ['text-summary'],
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/libs/internal/test-util/tsconfig.spec.json',
