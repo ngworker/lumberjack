1ce33cbf755a6a622059d9ca4f4e5bb39edb8905
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-11-07 01:20:35 +0100

    test: remove generated login command

--- e2e/docs/lumberjack-docs-app-e2e/src/integration/app.spec.ts
+++ e2e/docs/lumberjack-docs-app-e2e/src/integration/app.spec.ts
@@ -4,9 +4,6 @@ describe('docs-lumberjack-docs-app', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
     // Function helper example, see `../support/app.po.ts` file
     getGreeting().contains('Welcome docs-lumberjack-docs-app');
   });
