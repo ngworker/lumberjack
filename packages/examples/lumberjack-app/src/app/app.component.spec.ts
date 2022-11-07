eaa7c95e645e70ba019d0af12c4f2b17b34e40a8
Nacho Vazquez <nachovc1410@gmail.com>
2022-11-07 12:14:03 -0300

    fix: failing tests

--- packages/examples/lumberjack-app/src/app/app.component.spec.ts
+++ packages/examples/lumberjack-app/src/app/app.component.spec.ts
@@ -3,13 +3,11 @@ import { TestBed } from '@angular/core/testing';
 import { LumberjackModule } from '@ngworker/lumberjack';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AppComponent, NxWelcomeComponent],
-      imports: [LumberjackModule.forRoot()],
+      imports: [AppComponent, LumberjackModule.forRoot()],
     }).compileComponents();
   });
 
