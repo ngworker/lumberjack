663d46d2b83896e8b78904ac8643c0301eec47ef
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-11-29 21:01:26 +0100

    chore: align Cypress setup with fresh Nx 15.2 workspace

--- e2e/examples/lumberjack-app-e2e/cypress.config.ts
+++ e2e/examples/lumberjack-app-e2e/cypress.config.ts
@@ -1,19 +1,6 @@
-import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
+import { defineConfig } from 'cypress';
 
-const cypressJsonConfig = {
-  fileServerFolder: '.',
-  fixturesFolder: './src/fixtures',
-  video: true,
-  videosFolder: '../../../dist/cypress/e2e/examples/lumberjack-app-e2e/videos',
-  screenshotsFolder: '../../../dist/cypress/e2e/examples/lumberjack-app-e2e/screenshots',
-  chromeWebSecurity: false,
-  specPattern: 'src/e2e/**/*.cy.{js,jsx,ts,tsx}',
-  supportFile: 'src/support/e2e.ts',
-};
 export default defineConfig({
-  e2e: {
-    ...nxE2EPreset(__dirname),
-    ...cypressJsonConfig,
-  },
+  e2e: nxE2EPreset(__dirname),
 });
