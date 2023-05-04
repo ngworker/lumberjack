f7e316fb64a8de5e572f9c9cfd53ecc71b5d44b0
Nacho Vazquez <nachovc1410@gmail.com>
2023-05-03 22:15:48 -0300

    fix: check for it isFunction instead of isClass for export functions

--- packages/ngworker/lumberjack/console-driver/src/configuration-api.spec.ts
+++ packages/ngworker/lumberjack/console-driver/src/configuration-api.spec.ts
@@ -1,4 +1,4 @@
-import { isClass } from '@internal/test-util';
+import { isClass, isFunction } from '@internal/test-util';
 
 import {
   LumberjackConsoleDriverModule,
@@ -25,7 +25,7 @@ describe('Configuration API', () => {
     it(`exposes ${provideLumberjackConsoleDriver.name}`, () => {
       const sut = provideLumberjackConsoleDriver;
 
-      expect(isClass(sut)).toBeTruthy();
+      expect(isFunction(sut)).toBeTruthy();
     });
   });
 });
