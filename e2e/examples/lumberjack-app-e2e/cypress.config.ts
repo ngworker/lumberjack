f964d620ffa96b73bceac54739639c0a0ae787f7
Nacho Vazquez <nachovc1410@gmail.com>
2023-05-15 21:03:02 -0300

    chore: migrate to nx 16.1 (#153)
    ## PR Checklist
    
    Please check if your PR fulfills the following requirements:
    
    - [ ] The commit message follows our guidelines: CONTRIBUTING.md#commit
    - [ ] Tests for the changes have been added (for bug fixes / features)
    - [ ] Docs have been added / updated (for bug fixes / features)
    
    ## PR Type
    
    What kind of change does this PR introduce?
    
    <!-- Please check the one that applies to this PR using "x". -->
    
    ```
    [ ] Bugfix
    [ ] Feature
    [ ] Code style update (formatting, local variables)
    [ ] Refactoring (no functional changes, no api changes)
    [x] Build related changes
    [ ] CI related changes
    [ ] Documentation content changes
    [ ] Other... Please describe:
    ```
    
    ## What is the new behavior?
    
    We have upgraded to the latest version of Nx, 16.1
    
    ## Does this PR introduce a breaking change?
    
    ```
    [ ] Yes
    [x] No
    ```

--- e2e/examples/lumberjack-app-e2e/cypress.config.ts
+++ e2e/examples/lumberjack-app-e2e/cypress.config.ts
@@ -1,6 +1,8 @@
-import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  e2e: nxE2EPreset(__dirname),
+  e2e: {
+    ...nxE2EPreset(__dirname),
+  },
 });
