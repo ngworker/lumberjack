331f64b1631b876385e2e3767d52b5c108bbcd6c
Nacho Vazquez <nachovc1410@gmail.com>
2024-01-07 22:19:11 -0300

    fix: format issues

--- packages/internal/test-util/src/lib/error-throwing-driver/provide-error-throwing-driver.ts
+++ packages/internal/test-util/src/lib/error-throwing-driver/provide-error-throwing-driver.ts
@@ -31,6 +31,6 @@ export function provideErrorThrowingDriver(config: Partial<ErrorThrowingDriverCo
       }),
       deps: [lumberjackLogDriverConfigToken],
     },
-    errorThrowingDriverProvider
+    errorThrowingDriverProvider,
   ];
 }
