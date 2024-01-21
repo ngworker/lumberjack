bc722afa86acc754a38cb14ae09dcad0de279c3c
Nacho Vazquez <nachovc1410@gmail.com>
2024-01-21 12:14:47 -0300

    feat: add literal union levels and depricate enum levels

--- packages/ngworker/lumberjack/src/lib/logs/lumberjack-config-levels.ts
+++ packages/ngworker/lumberjack/src/lib/logs/lumberjack-config-levels.ts
@@ -1,8 +1,23 @@
-import { LumberjackLevel } from './lumberjack-level';
-import { LumberjackLogLevel } from './lumberjack-log-level';
+import { Level, LumberjackLevel } from './lumberjack-level';
+import { LogLevel, LumberjackLogLevel } from './lumberjack-log-level';
 
 /**
  * A set of Levels used to configure a log driver. Lumberjack filters logs
  * passed to the log driver based on its configured log levels.
+ *
+ * @deprecated use `ConfigLevels` instead. Enums will be removed in Lumberjack 19 in favor of string literal types.
  */
 export type LumberjackConfigLevels = LumberjackLogLevel[] | [LumberjackLevel.Verbose];
+
+/**
+ * A set of Levels used to configure a log driver. Lumberjack filters logs
+ * passed to the log driver based on its configured log levels.
+ *
+ * We recommend aliasing `ConfigLevels` to `LumberjackConfigLevels`. This will be the final name after removing enum based levels.
+ *
+ * @example
+ * ```typescript
+ * import { ConfigLevels as LumberjackConfigLevels } from '@ngworker/lumberjack';
+ * ```
+ */
+export type ConfigLevels = LogLevel[] | [Extract<Level, 'verbose'>];
--- packages/ngworker/lumberjack/src/lib/logs/lumberjack-level.ts
+++ packages/ngworker/lumberjack/src/lib/logs/lumberjack-level.ts
@@ -1,5 +1,8 @@
 /**
- * Level used to configure log drivers or add a log level to a log.
+ * All supported levels supported by Lumberjack. Used to specify the severity of a log and to configure Lumberjack and the log drivers.
+ *
+ * @deprecated use `Level` instead. Enums will be removed in Lumberjack 19 in favor of string literal types.
+ *
  */
 export enum LumberjackLevel {
   Critical = 'critical',
@@ -10,3 +13,15 @@ export enum LumberjackLevel {
   Verbose = 'verbose',
   Warning = 'warn',
 }
+
+/**
+ * All supported levels supported by Lumberjack. Used to specify the severity of a log and to configure Lumberjack and the log drivers.
+ *
+ * We recommend aliasing `Level` to `LumberjackLevel`. This will be the final name after removing enum based levels.
+ *
+ * @example
+ * ```typescript
+ * import { Level as LumberjackLevel } from '@ngworker/lumberjack';
+ * ```
+ */
+export type Level = 'critical' | 'debug' | 'error' | 'info' | 'trace' | 'verbose' | 'warn';
