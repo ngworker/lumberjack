b69208a7a4631e416d44b9f9b1fa9b6820f9a884
Nacho Vazquez <nacho.vazquez.dev@gmail.com>
2024-05-28 17:26:41 -0300

    fix: adjust cypress config

--- e2e/examples/lumberjack-app-e2e/cypress.config.ts
+++ e2e/examples/lumberjack-app-e2e/cypress.config.ts
@@ -3,6 +3,14 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    ...nxE2EPreset(__dirname),
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      webServerCommands: {
+        default: 'nx run examples-lumberjack-app:serve:development',
+        production: 'nx run examples-lumberjack-app:serve:production',
+      },
+      ciWebServerCommand: 'nx run examples-lumberjack-app:serve-static',
+    }),
+    baseUrl: 'http://localhost:4200',
   },
 });
