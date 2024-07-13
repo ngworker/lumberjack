e9b48563c9468069db6fa5a986ff34be22f55c3b
Nacho Vazquez <nacho.vazquez.dev@gmail.com>
2024-07-13 19:41:20 -0300

    feat: remove depricated ngmodules

--- packages/ngworker/lumberjack/src/lib/configuration/lumberjack-root.module.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expectNgModuleToBeGuardedAgainstDuplicateRegistration } from '@internal/test-util';
-
-import { LumberjackRootModule } from './lumberjack-root.module';
-
-describe(LumberjackRootModule.name, () => {
-  it('guards against being registered in multiple injectors', async () => {
-    await expectNgModuleToBeGuardedAgainstDuplicateRegistration(LumberjackRootModule);
-  });
-});
