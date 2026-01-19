ec24056a686c7e277f265d5f9d771d3c4ecbd729
Nacho Vazquez <nacho@naxo.dev>
2026-01-19 11:14:23 -0300

    feat(core)!: upgrade to the latest angular and nx

--- packages/examples/lumberjack-app/jest.config.ts
+++ packages/examples/lumberjack-app/jest.config.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-export default {
+module.exports = {
   displayName: 'examples-lumberjack-app',
   preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
